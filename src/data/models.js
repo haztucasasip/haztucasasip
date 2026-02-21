bd1f971fcf68bcf8999109123f3becc8adbdb45e
YeremGA <yeremiguerrero2001@gmail.com>
2026-02-21 01:24:01 -0300

    Actualizando página Haz Tu Casa

---
+++ src/data/models.js
@@ -0,0 +1,139 @@
+export const models = [
+  {
+    slug: "alpina50",
+    name: "Vivienda Alpina 50m²",
+    price: "$10.000.000",
+
+    meters: 50,
+    terrace: 20,
+    bedrooms: 2,
+    bathrooms: 1,
+
+    heroImage: "/alpina/Alpina.png",
+
+    gallery: [
+        "/alpina/Alpina.png",     // imagen grande
+        "/alpina/Alpina2.png",    // imagen derecha arriba
+        "/alpina/Alpina3.png",    // imagen derecha abajo
+        "/alpina/planta1.png"    // imagen derecha abajo
+    ],
+
+    description: `
+       
+        Una Vivienda Alpina prefabricada con paneles SIP, pensada para su facilidad y velocidad de montaje...
+        `,
+
+    specifications: [
+      "Muros exteriores de 120 mm Panel SIP = 32",
+      "Muros interiores de 90 mm Panel SIP = 9",
+      "Estructura de Refuerzo Perfilería de acero 150x50x3mm = 4",
+      "Cercha de techo de Madera o Acero galvanizado",
+      "Solera Superior e Inferior Perfil de madera 2x4 = 14",
+      "Montantes Perfiles de Madera 3x4 = 50",
+      "Cielo con estructura de madera o acero",
+      "Techo de Zinc acanalado o Corte en V",
+      "Hojalatería completa de techumbre",
+      "Puerta principal de madera de Pino sólido con vidrio",
+      "Puertas interiores de madera de Pino",
+      "Ventanas y Ventanales de aluminio o Termopanel",
+      "Revestimiento interior volcanita 10 mm",
+      "Revestimiento zona húmeda super board 6 mm",
+      "Revestimiento exterior WPC color negro",
+      "Incluye planos técnicos + Planos de armado"
+    ]
+  },
+  
+  {
+    slug: "mediterranea50",
+    name: "Vivienda Mediterránea 50m²",
+    price: "$--.--.--",
+
+    meters: 50,
+    terrace: 20,
+    bedrooms: 2,
+    bathrooms: 1,
+
+    heroImage: "/medi50/Medi.png",
+
+    gallery: [
+        "/medi50/Medi.png",     // imagen grande
+        "/medi50/Medi2.png",    // imagen derecha arriba
+        "/medi50/Medi3.png",    // imagen derecha abajo
+        "/medi50/planta1.png"    // imagen derecha abajo
+    ], // opcional si tienes plano
+
+  description: `
+    Una Vivienda Mediterránea prefabricada con paneles SIP, pensada para su facilidad y velocidad de montaje lo que permite un proceso constructivo simple sin mayores complicaciones. Esta cuenta con plano de armado y manual de montaje además de tener plantas técnicas de la vivienda para acelerar el proceso de regularización.
+
+    La vivienda se articula en una zona amplia que combina living/comedor/cocina que se quiebra por un pasillo. Cuenta con 2 habitaciones amplias y un baño que divide ambos espacios. Sus ventanas permiten visual panorámica o en formato vertical, entregando iluminación natural y amplitud.
+    `,
+
+  specifications: [
+    "Muros exteriores de 120 mm Panel SIP = 28",
+    "Muros interiores de 90 mm Panel SIP = 14",
+    "Cercha de techo de madera o acero galvanizado",
+    "Solera superior e inferior perfil de madera 2x4 = 18",
+    "Montantes perfiles de madera 3x4 = 42",
+    "Cielo con estructura de madera grilla regular 2x2 o metalcom",
+    "Techo de zinc acanalado o corte en V",
+    "Hojalatería completa de techumbre",
+    "Puerta principal de pino sólido con vidrio (incluye cerradura)",
+    "Puertas interiores de madera de pino",
+    "Ventanas y ventanales de aluminio o termopanel",
+    "Revestimiento interior zonas secas en volcanita 10 mm + pintura a elección",
+    "Revestimiento interior zona húmeda en plancha super board 6 mm",
+    "Revestimiento exterior tinglado de madera",
+    "Costaneras de cielo de pino impregnado",
+    "Incluye planos técnicos + planos de armado"
+  ],
+
+    },
+  {
+    slug: "mediterranea80",
+    name: "Vivienda Mediterránea 80m²",
+    price: "$--.--.--",
+
+    meters: 80,
+    terrace: 20,
+    bedrooms: 3,
+    bathrooms: 2,
+
+    heroImage: "/medi80/vista1.png",
+
+    gallery: [
+        "/medi80/vista1.png",     // imagen grande
+        "/medi80/vista2.png",    // imagen derecha arriba
+        "/medi80/vista3.png",    // imagen derecha abajo
+        "/medi80/planta.png"    // imagen derecha abajo
+    ], // opcional si tienes plano
+
+  description: `
+    
+    Una Vivienda Mediterránea prefabricada con paneles SIP, pensada para su facilidad y velocidad de montaje lo que permite un proceso constructivo simple sin mayores complicaciones. Esta cuenta con plano de armado y manual de montaje además de tener plantas técnicas de la vivienda para acelerar el proceso de regularización.
+
+    La vivienda se articula en una zona amplia que combina living/comedor/cocina que se quiebra por un pasillo. Cuenta con 2 habitaciones amplias y un baño que divide ambos espacios. Sus ventanas permiten visual panorámica o en formato vertical, entregando iluminación natural y amplitud.
+    `,
+
+  specifications: [
+    "Muros exteriores de 120 mm Panel SIP = 28",
+    "Muros interiores de 90 mm Panel SIP = 14",
+    "Cercha de techo de madera o acero galvanizado",
+    "Solera superior e inferior perfil de madera 2x4 = 18",
+    "Montantes perfiles de madera 3x4 = 42",
+    "Cielo con estructura de madera grilla regular 2x2 o metalcom",
+    "Techo de zinc acanalado o corte en V",
+    "Hojalatería completa de techumbre",
+    "Puerta principal de pino sólido con vidrio (incluye cerradura)",
+    "Puertas interiores de madera de pino",
+    "Ventanas y ventanales de aluminio o termopanel",
+    "Revestimiento interior zonas secas en volcanita 10 mm + pintura a elección",
+    "Revestimiento interior zona húmeda en plancha super board 6 mm",
+    "Revestimiento exterior tinglado de madera",
+    "Costaneras de cielo de pino impregnado",
+    "Incluye planos técnicos + planos de armado"
+  ],
+
+    }
+
+
+];
